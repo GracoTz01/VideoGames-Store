ac2defa6bf6043c7a4d964309a9aa3c5f659fea1
GracoTz01 <grabielacostacruz01@gmail.com>
2025-07-28 21:53:34 -0500

    rutas nuevas agregadas

--- Backend/routes/index.js
+++ Backend/routes/index.js
@@ -1,10 +1,11 @@
 // Routes here
 const express = require('express');
-const authRoutes = require('./auth');
 const router = express.Router();
 
 // Use auth routes
 router.use('/auth', require('./auth'));
+router.use('/user', require('./user'));
+router.use('/products', require('./products'));
 
 // Export the main router
 module.exports = router;
