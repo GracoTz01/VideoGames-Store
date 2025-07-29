424d1657d6230db783036ffab5b01012e8418e01
GracoTz01 <grabielacostacruz01@gmail.com>
2025-07-28 20:23:56 -0500

    Base de Datos y Server conectados

---
+++ Backend/index.js
@@ -0,0 +1,22 @@
+require('dotenv').config();
+
+// Import necessary modules
+const express = require('express');
+const { connectDB } = require('./connection/connect');
+const cors = require('cors');
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+// Middleware to parse JSON requests
+app.use(cors());
+app.use(express.json());
+
+// Route imports
+// app.use('/api', require('./routes'));
+
+// Connect MongoDB and start the server
+connectDB().then(() => {
+    app.listen(PORT, () => {
+        console.log(`Server is running on port ${PORT}`);
+    });
+});
