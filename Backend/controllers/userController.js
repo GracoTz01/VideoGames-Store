827c443f13609ad039f8fa4ad906685541d6707a
GracoTz01 <grabielacostacruz01@gmail.com>
2025-07-28 21:53:08 -0500

    /deposit listo

---
+++ Backend/controllers/userController.js
@@ -0,0 +1,23 @@
+const User = require('../models/User');
+
+// Deposit money into user account
+const depositMoney = async (req, res) => {
+    const { amount } = req.body;
+    try {
+        if (amount <= 0) {
+            return res.status(400).json({ message: 'Amount must be greater than zero.' });
+        }
+        const user = await User.findById(req.user.id);
+        if (!user) {
+            return res.status(404).json({ message: 'User not found.' });
+        }
+        user.balance += amount;
+        await user.save();
+        res.status(200).json({ message: 'Deposit successful', balance: user.balance });
+    } catch (error) {
+        res.status(500).json({ message: 'Error depositing money', error });
+    }
+}
+
+
+module.exports = { depositMoney };
